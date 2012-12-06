55fd1801cc2f27d078539c784b17a06efee125de
David Collinson <hiroto@hiroto.eu>
2012-12-06 17:29:12 -0500

    On second thoughts, integrating twilio into the manager.

--- twilio_.py
+++
@@ -1,8 +0,0 @@
-import config
-if config.twilio:
-    try:
-        from twilio.rest import TwilioRestClient as Twilio
-    except ImportError:
-        print "Consider installing twilio's python REST API"
-    client = Twilio(config.account, config.token)
-    
